4fde2f49f22e127b08065755d41dbfa661c1b7dd
Joilson Sousa <191298450+joilsonsousa-s@users.noreply.github.com>
2025-02-04 13:05:37 -0300

    Criei a aula24

---
+++ aulas/Aula24/Aula24.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Aula24
+{
+    static void Main()
+    {
+        int v1, v2, res;
+
+        Console.Write("Digite um número...: ");
+        v1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite outro número: ");
+        v2 = int.Parse(Console.ReadLine());
+
+        res = soma(v1, v2);
+
+        Console.WriteLine($"A soma de {v1} e {v2} é igual a {res}");
+    }
+
+    static int soma(int n1, int n2)
+    {
+        int res = n1 + n2;
+        return res;
+    }
+}
