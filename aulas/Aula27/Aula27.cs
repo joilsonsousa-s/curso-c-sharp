6ffa4dee31837c7c6d6529806ba10d5b2c19069c
Joilson Sousa <191298450+joilsonsousa-s@users.noreply.github.com>
2025-02-04 14:59:13 -0300

    Criei a aula27

---
+++ aulas/Aula27/Aula27.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Aula27
+{
+    static void Main()
+    {
+        soma(5,7);
+    }
+
+    static void soma(params int[]n) {
+        int res = 0;
+
+        if(n.Length < 1) {
+            Console.WriteLine("Não existem valores a serem somados.");
+        } else if(n.Length < 2) {
+            Console.WriteLine($"Valores insuficientes para nossa soma: {n[0]}");
+        } else {
+            for(int i=0; i < n.Length; i++) {
+                res += n[i];
+            }
+            Console.WriteLine($"A soma dos valores é: {res}");
+        }
+    }
+}
