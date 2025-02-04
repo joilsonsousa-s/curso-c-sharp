81358c8ac4bbe7183e39277bc63fb990096c27a1
Joilson Sousa <191298450+joilsonsousa-s@users.noreply.github.com>
2025-02-04 14:27:08 -0300

    Criei a aula26

---
+++ aulas/Aula26/Aula26.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Aula26
+{
+    static void Main()
+    {
+        int divD, divS, quoc, rest;
+        divD = 10;
+        divS = 5;
+        quoc = divide(divD, divS, out rest);
+
+        Console.WriteLine($"{divD} / {divS} = {quoc} e resto {rest}");
+    }
+
+   static int divide(int dividendo, int divisor, out int resto)
+   {
+        int quociente;
+
+        quociente = dividendo / divisor;
+        resto = dividendo % divisor;
+
+        return quociente;
+   }
+}
